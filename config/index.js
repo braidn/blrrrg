dd74da9adc4e52b4aa29e14cb8f87dfe7a682219
Braden Douglass <braden.douglass@gmail.com>
2019-03-06 23:00:29 -0500

    Replace Defaults With Cloudbacon.

--- config/index.js
+++ config/index.js
@@ -1,26 +1,25 @@
 module.exports = {
-  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+  pathPrefix: "/", // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Minimal Blog', // Navigation and Site Title
-  siteTitleAlt: 'Minimal Blog - Gatsby Starter', // Alternative Site title for SEO
-  siteTitleManifest: 'MinimalBlog',
-  siteUrl: 'https://minimal-blog.lekoarts.de', // Domain of your site. No trailing slash!
-  siteLanguage: 'en', // Language Tag on <html> element
-  siteHeadline: 'Writing and publishing content', // Headline for schema.org JSONLD
-  siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
-  favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
-  siteDescription: 'Minimal Blog with a focus on typography. Gatsby Starter powered by MDX.', // Your site description
-  author: 'LekoArts', // Author for schemaORGJSONLD
-  siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
+  siteTitle: "Cloudbacon", // Navigation and Site Title
+  siteTitleAlt: "Cloudbacon - Ramblings of Braidn", // Alternative Site title for SEO
+  siteTitleManifest: "Cloudbacon",
+  siteUrl: "https://cloudbacon.com", // Domain of your site. No trailing slash!
+  siteLanguage: "en", // Language Tag on <html> element
+  siteHeadline: "Writing and publishing content", // Headline for schema.org JSONLD
+  siteBanner: "/social/banner.jpg", // Your image for og:image tag. You can find it in the /static folder
+  favicon: "src/favicon.png", // Your image for favicons. You can find it in the /src folder
+  siteDescription:
+    "Blog about Ruby/Reason/JavaScript and the professional world of programming", // Your site description
+  author: "Braidn", // Author for schemaORGJSONLD
+  siteLogo: "/social/logo.png", // Image for schemaORGJSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@minimal', // Twitter Username - Optional
-  ogSiteName: 'minimal', // Facebook Site Name - Optional
-  ogLanguage: 'en_US', // Facebook Language
-  googleAnalyticsID: 'UA-47519312-6',
+  userTwitter: "@braidn", // Twitter Username - Optional
+  googleAnalyticsID: "UA-47519312-6",
 
   // Manifest and Progress color
   // See: https://developers.google.com/web/fundamentals/web-app-manifest/
-  themeColor: '#3498DB',
-  backgroundColor: '#2b2e3c',
-}
+  themeColor: "#3498DB",
+  backgroundColor: "#2b2e3c"
+};
