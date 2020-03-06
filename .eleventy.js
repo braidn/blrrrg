4c7d1f2d49059e71e7a81cb5b39962ee8b28c035
Braden Douglass <braden.douglass@gmail.com>
2020-03-06 12:23:33 -0500

    Create A Feed On Build.
    Wire up the RSS plugin to build the feed when the site is built. This
    will create the feed under /feed. The use of Nunjucks here over Liquid
    is purely what is supported by the RSS plugin. This plus they share a
    lot of their syntax.

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = eleventyConfig => {
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPlugin(pluginRss);
 
   return {
     //Use ejs in html templates
