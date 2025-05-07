92b1833c4783d094f4aa22884be7cfd0a19006b1
Braden Douglass <braden.douglass@gmail.com>
2025-05-07 16:54:46 -0400

    feat: Lightning CSS Plugin For 11ty
    Utilize a plugin over concurrently/double build
    steps when serving the application. This also drastically
    speeds up the builds considerably.
    
    This fix currently only works in dev.

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,10 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const embedYouTube = require("eleventy-plugin-youtube-embed");
-
+const lightningCSS = require("@11tyrocks/eleventy-plugin-lightningcss");
 
 module.exports = eleventyConfig => {
+  eleventyConfig.addPlugin(lightningCSS);
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy(".well-known");
