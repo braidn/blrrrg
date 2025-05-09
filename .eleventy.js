a8b2df3c582762d4e57ec094c1406fceaa9969c7
Braden Douglass <braden.douglass@gmail.com>
2025-05-09 11:32:25 -0400

    refactor: Fix/Remove Use of Lightning CSS Imports.
    The imports need a bit of fixing/cutting and the
    package.json still references some old code that's
    no longer needed.

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,9 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const embedYouTube = require("eleventy-plugin-youtube-embed");
-const lightningCSS = require("@11tyrocks/eleventy-plugin-lightningcss");
-const manipulateCSS = require("./_helpers/css-compression.js");
+const { manipulateCSS } = require("./_helpers/css-compression.js");
 
 module.exports = eleventyConfig => {
-  eleventyConfig.addPlugin(lightningCSS);
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy(".well-known");
