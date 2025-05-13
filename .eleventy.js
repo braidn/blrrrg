b7c383ab6897ea881e8e4a4d5a81c77bac4a513a
Braden Douglass <braden.douglass@gmail.com>
2025-05-13 19:50:32 -0400

    feat: Opt For the Bundle Plugin.
    This is now built into 11ty as of version 3. This
    bundling allows us to build custom bundles of CSS/JS
    for the application.
    
    In this case, we bundle all of the imports together
    during DEV into a single file and inline in production.

--- .eleventy.js
+++ .eleventy.js
@@ -1,23 +1,23 @@
 import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import pluginRss from "@11ty/eleventy-plugin-rss";
 import embedYouTube from "eleventy-plugin-youtube-embed";
-import { manipulateCSS } from "./_helpers/css-compression.js";
 import markdownIt from "markdown-it";
+import pluginBundle from "@11ty/eleventy-plugin-bundle";
 import markdownFootnotes from "markdown-it-footnote";
 
 export default function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginBundle);
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy(".well-known");
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(embedYouTube);
+  eleventyConfig.addBundle("css")
 
   let options = {
     html: true
   };
   let markdownLib = markdownIt(options).use(markdownFootnotes);
-
-  manipulateCSS(eleventyConfig);
 
   eleventyConfig.setLibrary("md", markdownLib);
 
