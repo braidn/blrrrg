22411dd866f3096780bebb3d9fdaaced77e59cfc
Braden Douglass <braden.douglass@gmail.com>
2025-05-13 19:59:08 -0400

    feat: Minify CSS In Production.
    Use CleanCSS to minify the code in production.
    Utilize the transform functions available to the
    pluginBundle.
    
    The function makes sure that css is the type and
    that the correct env variable is set. If everything
    is good, minify and clean the code. This will produce
    a much smaller/svelter production set of inline code.

--- .eleventy.js
+++ .eleventy.js
@@ -4,9 +4,19 @@ import embedYouTube from "eleventy-plugin-youtube-embed";
 import markdownIt from "markdown-it";
 import pluginBundle from "@11ty/eleventy-plugin-bundle";
 import markdownFootnotes from "markdown-it-footnote";
+import CleanCSS from "clean-css";
 
 export default function(eleventyConfig) {
-  eleventyConfig.addPlugin(pluginBundle);
+  eleventyConfig.addPlugin(pluginBundle, {
+    transform: [
+      async function (code, _inputPath) {
+        if (this.type === "css" && process.env.ELEVENTY_ENV === "production") {
+          return (new CleanCSS).minify(code).styles;
+        }
+        return content;
+      }
+    ]
+  });
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy(".well-known");
