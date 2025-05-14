450eefad65a0748a45a37e8145778295e79fbd74
Braden Douglass <braden.douglass@gmail.com>
2025-05-13 20:27:00 -0400

    Fingerprinting CSS Assets. (#69)
    * feat: Lightning CSS Plugin For 11ty
    
    Utilize a plugin over concurrently/double build
    steps when serving the application. This also drastically
    speeds up the builds considerably.
    
    This fix currently only works in dev.
    
    * feat: Add A Script For Manipulating CSS.
    
    This script will take the expected CSS and create
    a hashed and compressed file for serving over Cloudflare
    and a normal CSS file in development.
    
    * feat: Utilize New CSS File in Templates.
    
    Add this new registered function into the eleventy
    config and utilize the output in special hooks in
    the templates.
    
    * refactor: Fix/Remove Use of Lightning CSS Imports.
    
    The imports need a bit of fixing/cutting and the
    package.json still references some old code that's
    no longer needed.
    
    * feat: Lightning CSS In Brotli Helper.
    
    Instead of using CleanCSS to transform the CSS,
    utilize Lightning. This dependency was always hanging
    around and ultimately it's the best way forward with
    CSS at the moment.
    
    Utilize it's ability to minimize the CSS when being
    deployed to prod.
    
    * fix: Builds Without Explicit Lightning Scripts.
    
    Clean up the manual Lightning builds for both the
    dev and production deploys.
    
    * feat: ES6 Module This Thing Everywhere.
    
    Move this all over to a module approach using es6
    throughout the eleventy config.
    
    * feat: Output Cache Key In Logs.
    
    This console.log should run when the application
    initially builds the cache key. This should aid
    in debugging why this isn't being created correctly
    on Cloudflare.
    
    * chore: Ignore Manifest Files.
    
    Manifest files for CSS busting. Ignore these locally
    since they will be run in development. Production
    will inline the css for speed and POWER.
    
    * feat: Opt For the Bundle Plugin.
    
    This is now built into 11ty as of version 3. This
    bundling allows us to build custom bundles of CSS/JS
    for the application.
    
    In this case, we bundle all of the imports together
    during DEV into a single file and inline in production.
    
    * feat: Minify CSS In Production.
    
    Use CleanCSS to minify the code in production.
    Utilize the transform functions available to the
    pluginBundle.
    
    The function makes sure that css is the type and
    that the correct env variable is set. If everything
    is good, minify and clean the code. This will produce
    a much smaller/svelter production set of inline code.

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,29 @@
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const embedYouTube = require("eleventy-plugin-youtube-embed");
+import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
+import pluginRss from "@11ty/eleventy-plugin-rss";
+import embedYouTube from "eleventy-plugin-youtube-embed";
+import markdownIt from "markdown-it";
+import pluginBundle from "@11ty/eleventy-plugin-bundle";
+import markdownFootnotes from "markdown-it-footnote";
+import CleanCSS from "clean-css";
 
-
-module.exports = eleventyConfig => {
+export default function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginBundle, {
+    transform: [
+      async function (code, _inputPath) {
+        if (this.type === "css" && process.env.ELEVENTY_ENV === "production") {
+          return (new CleanCSS).minify(code).styles;
+        }
+        return content;
+      }
+    ]
+  });
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy(".well-known");
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(embedYouTube);
+  eleventyConfig.addBundle("css")
 
-  let markdownIt = require("markdown-it");
-  let markdownFootnotes = require('markdown-it-footnote')
   let options = {
     html: true
   };
@@ -28,4 +40,5 @@ module.exports = eleventyConfig => {
       "njk"
     ]
   };
-};
+}
+
---
+++ _helpers/css-compression.js
@@ -0,0 +1,85 @@
+import zlib from 'zlib';
+import dotenv from "dotenv";
+import fs from 'fs';
+import crypto from 'crypto';
+import path from 'path';
+import { transform } from 'lightningcss';
+dotenv.config();
+
+// Default Brotli compression level if not set in the environment
+const DEFAULT_BROTLI_COMPRESSION_LEVEL = 6;
+
+export function manipulateCSS(eleventyConfig) {
+	eleventyConfig.addShortcode("customCSS", async function(cssPath) {
+		if (process.env.ELEVENTY_ENV === 'development') {
+			return `<link rel="stylesheet" href="${cssPath}">`;
+		}
+
+		const inputFile = path.join('./public', cssPath);
+		const outputDirectory = path.join('./_site', 'css');
+		const cacheDirectory = path.join('./.cache', 'css');
+
+		// Get compression level from the environment or use the default
+		const brotliCompressionLevel = parseInt(process.env.BROTLI_COMPRESSION_LEVEL || DEFAULT_BROTLI_COMPRESSION_LEVEL, 10);
+
+		try {
+			if (!fs.existsSync(inputFile)) {
+				console.error(`Input CSS file not found: ${inputFile}`);
+				return '';
+			}
+
+			for (const dir of [cacheDirectory, outputDirectory]) {
+				if (!fs.existsSync(dir)) {
+					fs.mkdirSync(dir, { recursive: true });
+				}
+			}
+
+			const inputCSS = await fs.promises.readFile(inputFile, 'utf8');
+			const hash = crypto.createHash('sha256').update(inputCSS).digest('hex').slice(0, 10);
+
+			const cacheKey = `${hash}-${cssPath.replace(/[\/\\]/g, '-')}`;
+			const cachePath = path.join(cacheDirectory, cacheKey);
+
+      console.log("New CSS file:", cacheKey);
+
+      let processedCSS;
+			if (fs.existsSync(cachePath)) {
+				processedCSS = await fs.promises.readFile(cachePath, 'utf8');
+			} else {
+        const result = transform({
+          code: inputCSS,
+          minify: true,
+        });
+        processedCSS = result.code
+				await fs.promises.writeFile(cachePath, processedCSS);
+			}
+
+			const parsedPath = path.parse(inputFile);
+			const finalFilename = path.join(outputDirectory, `${parsedPath.name}-${hash}${parsedPath.ext}`);
+			await fs.promises.writeFile(finalFilename, processedCSS);
+
+			// Brotli compression
+
+			// The output filename for the compressed file
+			const brotliFilename = `${finalFilename}.br`;
+
+			// Only compress to Brotli if the file doesn't exist
+			if (!fs.existsSync(brotliFilename)) {
+				// Set our zlib options here e.g. compression
+				const brotliOptions = {
+					level: brotliCompressionLevel // Use the level specified in the environment
+				};
+				// zlib does it's compression magic!
+				const brotliBuffer = zlib.brotliCompressSync(Buffer.from(processedCSS), brotliOptions);
+				// Write the compressed code to the output filename defined above
+				await fs.promises.writeFile(brotliFilename, brotliBuffer);
+			}
+
+			const hashedPath = brotliFilename.replace(path.join('./_site'), '').replace(/\\/g, '/');
+			return `<link rel="stylesheet" href="${hashedPath}">`;
+		} catch (err) {
+			console.error("Error processing CSS:", err);
+			return "";
+		}
+	});
+}
